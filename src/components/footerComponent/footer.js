32cff65a6126681af0828c4ba0f74c87dcb23d0a
Rayven <rayven.rey@gmail.com>
2022-03-15 22:13:48 +0100

    homepage / landing page done

--- src/components/footerComponent/footer.js
+++ src/components/footerComponent/footer.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import logo from '../headerComponent/logo.png';
+import apple from './app store.png';
+import google from './google-play.png';
+import './footer.scss';
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className='container-footer'>
+      <div className='logo-footer'>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className='links'>
+        <h2>Home</h2>
+        <h2>Terms of Use</h2>
+        <h2>Legal Notices</h2>
+        <h2>Help</h2>
+        <h2>Manage Account</h2>
+      </div>
+      <div className='store'>
+        <img src={apple} alt="app store" />
+        <img src={google} alt="google play" />
+      </div>
+      <div className='copyright'>
+        <h2>Copyright 2020 Dreadful Tomato Streaming. All Rights Reserved</h2>
+      </div>
     </div>
   )
 }
