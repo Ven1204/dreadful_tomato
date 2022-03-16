8f4509afd324e2e10d5d32a560b5e851886b0528
Rayven <rayven.rey@gmail.com>
2022-03-16 01:49:27 +0100

    fixed footer component render on moviePage component

--- src/components/moviesComponent/movie.js
+++ src/components/moviesComponent/movie.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import SearchBar from '../headerComponent/searchbar';
+import './movie.scss';
+import Footer from '../footerComponent/footer';
 
 const MoviePage = () => {
   return(
     <div>
-      <SearchBar />
+      <div className='movie-container'>
+        <div>
+        <SearchBar />
+        </div>
+
+        <div className='movie-cards-container'>
+          <div>
+
+          </div>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   )
 }
